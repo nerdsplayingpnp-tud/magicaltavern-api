538f52346cad527addb7f32d01941c8b15b8d41c
bitfl0wer <fleeprivate@gmail.com>
2022-08-18 15:31:27 +0200

    I am so done mentally In the last few commits i have - implemented a database handler - implemented the api key handler - lost my mind completely i am so sorry for stupid commit messages. i hope my code comments can explain everything.

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from flask import *
 from handle_apikeys import validate
+from pathlib import Path
+from db import Database, make_file
 import json, time
 
 app = Flask(__name__)
 
+# example_dict = {
+#     1: {"name": "2", "min_players": 1, "max_players": 5, "description": "lorem ipsum"},
+#     2: {"name": "1", "min_players": 2, "max_players": 5, "description": "lorem ipsum"},
+#     3: {"name": "3", "min_players": 3, "max_players": 5, "description": "lorem ipsum"},
+# }
 
-@app.route("/", methods=["GET"])
+# print(example_dict.get(1))
+
+
+@app.route("/api/", methods=["GET"])
 def home_page():
-    data_set = {"Page": "Home", "Message": "Loaded Homepage.", "Time": time.time()}
+    data_set = {
+        "Page": "Home",
+        "Message": "Loaded Homepage.",
+        "Current Time": time.time(),
+    }
     return json.dumps(data_set)
 
 
-@app.route("/user/", methods=["GET"])
-def request_page():
+@app.route("/api/v1.0/campaigns/", methods=["GET"])
+def get_campaigns():
     user_query = str(request.args.get("user"))  #  /user/?user=HASJK4AF_NAME
 
     data_set = {
@@ -24,5 +38,4 @@ def request_page():
 
 
 if __name__ == "__main__":
-    validate("a")
     app.run(port=7777)
