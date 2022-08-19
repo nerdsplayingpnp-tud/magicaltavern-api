08b2dabccbef1cf42b5c05abfa161be3b3bc03f5
bitfl0wer <fleeprivate@gmail.com>
2022-08-19 17:11:34 +0200

    Clean up main.py

--- main.py
+++ main.py
@@ -7,17 +7,11 @@ from api.v1_0.campaigns import campaigns_api
 import json, time
 
 app = Flask(__name__)
+# Register the blueprint in api/v1_0/campaigns.py
 app.register_blueprint(campaigns_api)
 
-# example_dict = {
-#     1: {"name": "2", "min_players": 1, "max_players": 5, "description": "lorem ipsum"},
-#     2: {"name": "1", "min_players": 2, "max_players": 5, "description": "lorem ipsum"},
-#     3: {"name": "3", "min_players": 3, "max_players": 5, "description": "lorem ipsum"},
-# }
 
-# print(example_dict.get(1))
-
-
+# Register a route
 @app.route("/api/v1.0/", methods=["GET"])
 def home_page():
     return json.dumps("API is online.")
