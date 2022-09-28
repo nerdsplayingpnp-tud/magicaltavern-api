653ccbd95f55e3f7e6252518fae21c6bc7857398
bitfl0wer <fleeprivate@gmail.com>
2022-09-28 19:45:58 +0200

    Include app_configurator.py in .gitignore

--- app_configurator.py
+++ app_configurator.py
@@ -4,13 +4,11 @@ from flask import Flask
 
 
 def configure(app: Flask) -> Flask:
-    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_SERVER"] = ""
     app.config["MAIL_PORT"] = 465
-    app.config["MAIL_USERNAME"] = "nerdsplaypnpvalidator@gmail.com"
-    app.config["MAIL_PASSWORD"] = "xwebtezpptgxfcbs"  #'$52ceP^1xbMU'
+    app.config["MAIL_USERNAME"] = ""
+    app.config["MAIL_PASSWORD"] = ""
     app.config["MAIL_USE_TLS"] = False
-    app.config["MAIL_USE_SSL"] = True
-    app.config[
-        "SECRET_KEY"
-    ] = "e6405ba489b12b5c3e736e70094c315c85132c5cb16bd2c5cfbbbe47868bfe32"
+    app.config["MAIL_USE_SSL"] = False
+    app.config["SECRET_KEY"] = ""
     return app
