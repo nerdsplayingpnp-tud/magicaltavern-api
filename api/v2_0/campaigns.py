560fdeee9f15f5de62a2f9a4d7e6c0bf0fdf2480
bitfl0wer <fleeprivate@gmail.com>
2022-11-05 21:47:07 +0100

    TODO: Work on Campaigns V2 API

--- api/v2_0/campaigns.py
+++ api/v2_0/campaigns.py
@@ -1,8 +1,11 @@
 from api.v2_0.models import dbsql as db
 from flask import Flask, Blueprint, abort, jsonify, request, views
+from api.v2_0.models import Campaign
 
-campaigns = Blueprint("campaigns_api_v2", __name__)
+campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 
-@campaigns.route("/api/v2.0/campaigns/", methods=["GET"])
+
+@campaigns_api_v2.route("/api/v2.0/campaigns/", methods=["GET"])
 def get_all_campaigns():
-
+    query_campaigns = Campaign.query.all()
+    return jsonify(query_campaigns), 200
