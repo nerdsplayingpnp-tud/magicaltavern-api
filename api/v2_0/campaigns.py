6288b34e9bbb4757db271b21605b4b2d39c3dd84
bitfl0wer <fleeprivate@gmail.com>
2022-11-06 20:56:11 +0100

    Add 404 response

--- api/v2_0/campaigns.py
+++ api/v2_0/campaigns.py
@@ -1,6 +1,6 @@
 from api.v2_0.models import dbsql as db
-from flask import Flask, Blueprint, abort, jsonify, request, views
-from api.v2_0.models import Campaign
+from flask import Flask, Blueprint, abort, jsonify, request, views, Response
+from api.v2_0.models import Campaign, to_dict
 
 campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 
@@ -8,4 +8,4 @@ campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 @campaigns_api_v2.route("/api/v2.0/campaigns/", methods=["GET"])
 def get_all_campaigns():
     query_campaigns = Campaign.query.all()
-    return jsonify(query_campaigns), 200
+    return Response(status=404)  # TODO Implement
