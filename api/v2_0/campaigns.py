83e62f39ccac9745212797e721a57c2fe63ac789
bitfl0wer <fleeprivate@gmail.com>
2022-11-07 22:48:17 +0100

    get_all_campaigns() now fulfills intended purpose.

--- api/v2_0/campaigns.py
+++ api/v2_0/campaigns.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, Response
-from api.v2_0.models import Campaign
+from flask import Blueprint, request, jsonify
+from api.v2_0.authentication import abort_if_token_invalid
+from api.v2_0.models import table_to_dict, Campaign
 
 campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 
 
 @campaigns_api_v2.route("/api/v2.0/campaigns/", methods=["GET"])
 def get_all_campaigns():
-    query_campaigns = Campaign.query.all()
-    return Response(status=404)  # TODO Implement
+    abort_if_token_invalid(request)
+    return jsonify(table_to_dict(Campaign)), 200
