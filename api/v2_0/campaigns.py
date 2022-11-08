0b09d7f5c2bc085d677ca08567f4a8ba2f6666a8
bitfl0wer <fleeprivate@gmail.com>
2022-11-08 20:26:37 +0100

    get_singular_campaign() works.

--- api/v2_0/campaigns.py
+++ api/v2_0/campaigns.py
@@ -9,3 +9,10 @@ campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 def get_all_campaigns():
     abort_if_token_invalid(request)
     return jsonify(table_to_dict(Campaign)), 200
+
+
+@campaigns_api_v2.route("/api/v2.0/campaigns/<int:id>", methods=["GET"])
+def get_singular_campaign(id):
+    abort_if_token_invalid(request)
+    item = Campaign.query.filter(Campaign.id == id).one()
+    return jsonify(item.to_dict()), 200
