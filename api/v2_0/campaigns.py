d16a0d63f73edb7788b0629861711c49c37d601c
bitfl0wer <fleeprivate@gmail.com>
2022-11-13 00:43:46 +0100

    Add: remove player from campaign, get players from campaign TODO: jsonify get players from campaign-output and write get dm from campaign

--- api/v2_0/campaigns.py
+++ api/v2_0/campaigns.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, request, jsonify, escape, abort, Response
+from flask import Blueprint, request, jsonify, escape, abort
 from api.v2_0.authentication import abort_if_token_invalid
 from api.v2_0.models import (
     table_to_dict,
     Campaign,
     ensure_player_exists,
+    User,
     campaign_player_association,
 )
 from api.v2_0.models import dbsql as db
+
 
 campaigns_api_v2 = Blueprint("campaigns_api_v2", __name__)
 
@@ -62,6 +64,35 @@ def get_singular_campaign(id):
 
 
 @campaigns_api_v2.route(
+    "/api/v2.0/campaigns/<int:campaign_id>/players", methods=["GET"]
+)
+def get_players_from_campaign(campaign_id):
+    abort_if_token_invalid(request)
+    items = (
+        User.query.join(campaign_player_association)
+        .join(Campaign)
+        .filter(
+            (campaign_player_association.c.player == User.id)
+            & (campaign_player_association.c.campaign == campaign_id)
+        )
+        .all()
+    )
+    if not items:
+        abort(400, "This Campaign does not exist.")
+
+    return jsonify(items), 200
+
+
+""" @campaigns_api_v2.route("/api/v2.0/campaigns/<int:id>/dm", methods=["GET"])
+def get_dm_from_campaign(id):
+    abort_if_token_invalid(request)
+    item = Campaign.query.filter(Campaign.id == id).one_or_none()
+    if not item:
+        abort(400, "This Campaign does not exist.")
+    return jsonify(item.to_dict()), 200 """
+
+
+@campaigns_api_v2.route(
     "/api/v2.0/campaigns/<int:campaign_id>/players/add/<int:user_id>", methods=["PUT"]
 )
 def add_player_to_campaign(campaign_id, user_id):
@@ -73,5 +104,22 @@ def add_player_to_campaign(campaign_id, user_id):
     if user_from_id in campaign.players:
         abort(409, "The player is already in this campaign.")
     campaign.players.append(user_from_id)
+    db.session.commit()
+    return jsonify("Success"), 201
+
+
+@campaigns_api_v2.route(
+    "/api/v2.0/campaigns/<int:campaign_id>/players/remove/<int:user_id>",
+    methods=["PUT"],
+)
+def remove_player_from_campaign(campaign_id, user_id):
+    abort_if_token_invalid(request)
+    user_from_id = ensure_player_exists(user_id)
+    campaign = Campaign.query.filter(Campaign.id == campaign_id).one_or_none()
+    if not campaign:
+        abort(400, "This Campaign does not exist.")
+    if user_from_id not in campaign.players:
+        abort(409, "The player is not in this campaign.")
+    campaign.players.remove(user_from_id)
     db.session.commit()
     return jsonify("Success"), 201
