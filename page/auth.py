174478646f9392f5ff818882b2203e30e4e4b864
bitfl0wer <florian@pro-weber.com>
2022-11-30 14:51:37 +0100

    Really basic discord login

--- page/auth.py
+++ page/auth.py
@@ -1,5 +1,5 @@
 from main import discord, app
-from flask import request, abort, Blueprint
+from flask import request, abort, Blueprint, redirect
 from api.v2_0.models import dbsql as db
 from api.v2_0.models import User
 
@@ -9,3 +9,28 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login")
 def login():
     return discord.create_session()
+
+
+@auth.route("/callback")
+def callback():
+    data = discord.callback()
+    redirect_to = data.get("redirect", "/")
+
+    user = discord.fetch_user()
+    return redirect(redirect_to)
+
+
+@auth.route("/me")
+def me():
+    user = discord.fetch_user()
+    return f"""
+<html>
+<head>
+<title>{user.name}</title>
+</head>
+<body><img src='{user.avatar_url or user.default_avatar_url}' />
+<p>Is avatar animated: {str(user.is_avatar_animated)}</p>
+<br />
+</body>
+</html>
+"""
