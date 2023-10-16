d19b6e4e3e902bdfa87f759657407381bc35a9d9
bitfl0wer <florian@pro-weber.com>
2023-10-16 21:26:14 +0200

    Provide more command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use actix_web::{get, web, App, HttpServer, Responder};
-use clap::Parser;
+use std::fmt::Display;
+
+use actix_web::{get, App, HttpServer, Responder};
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug, Clone, Copy)]
+enum DatabaseType {
+    SQLite,
+    Postgres,
+    MySQL,
+}
+
+impl Display for DatabaseType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DatabaseType::SQLite => write!(f, "sqlite"),
+            DatabaseType::Postgres => write!(f, "postgres"),
+            DatabaseType::MySQL => write!(f, "mysql"),
+        }
+    }
+}
+
+impl ValueEnum for DatabaseType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::SQLite, Self::Postgres, Self::MySQL]
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            DatabaseType::SQLite => Some(clap::builder::PossibleValue::new("sqlite")),
+            DatabaseType::Postgres => Some(clap::builder::PossibleValue::new("postgres")),
+            DatabaseType::MySQL => Some(clap::builder::PossibleValue::new("mysql")),
+        }
+    }
+}
 
 /// magicaltavern API Server.
 #[derive(Parser, Debug)]
@@ -11,16 +44,17 @@ struct Args {
     /// Number of web workers to spawn. Defaults to the number of CPUs available.
     #[arg(short, long, default_value_t = num_cpus::get() as u16)]
     workers: u16,
+
+    #[arg(short = 'u', long)]
+    database_url: String,
+
+    #[arg(short, long, default_value_t = DatabaseType::SQLite)]
+    database_type: DatabaseType,
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+#[get("/api/v3")]
+async fn hello() -> impl Responder {
+    "The API is running!"
 }
 
 #[actix_web::main]
@@ -30,7 +64,7 @@ async fn main() -> std::io::Result<()> {
         "Starting magicaltavern-api with {} workers on port {}",
         args.workers, args.port
     );
-    HttpServer::new(|| App::new().service(index).service(hello))
+    HttpServer::new(|| App::new().service(hello))
         .workers(args.workers as usize)
         .bind(("127.0.0.1", args.port))?
         .run()
