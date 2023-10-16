95234d63950fca60be732aa91d5987b35067d35c
bitfl0wer <florian@pro-weber.com>
2023-10-16 19:59:33 +0200

    Simple example using actix_web and clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use clap::Parser;
+
+/// magicaltavern API Server.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+
+    /// Number of web workers to spawn. Defaults to the number of CPUs available.
+    #[arg(short, long, default_value_t = num_cpus::get() as u16)]
+    workers: u16,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    println!(
+        "Starting magicaltavern-api with {} workers on port {}",
+        args.workers, args.port
+    );
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .workers(args.workers as usize)
+        .bind(("127.0.0.1", args.port))?
+        .run()
+        .await
 }
